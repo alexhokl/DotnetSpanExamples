5ce853680693886c5d1d4b87ea68465f01daa83f
alexhokl <alexhokl@gmail.com>
2024-11-30 19:59:40 +0800

    Added benchmark tests

--- Program.cs
+++ Program.cs
@@ -1,7 +1,41 @@
-﻿internal class Program
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace DotnetSpanExamples;
+
+[MemoryDiagnoser]
+[HideColumns("Error", "StdDev", "Median", "RatioSD", "Gen0")]
+public class SpanTest
+{
+    public string Substring()
+    {
+        string str = "Hello, World!";
+        return str.Substring(7);
+    }
+
+    [Benchmark(Baseline = true)]
+    public string SubstringBenchmark()
+    {
+        return Substring();
+    }
+
+    public ReadOnlySpan<char> Slice()
+    {
+        ReadOnlySpan<char> span = "Hello, World!";
+        return span.Slice(7);
+    }
+
+    [Benchmark]
+    public string SliceBenchmark()
+    {
+        return Slice().ToString();
+    }
+}
+
+public class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var summary = BenchmarkRunner.Run<SpanTest>();
     }
 }
